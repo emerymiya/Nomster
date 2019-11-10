d4f458e6ef0fb7144cdab00bbde381e6b090428d
Emery Green <emerymiya@gmail.com>
2019-11-10 20:43:11 +0000

    Lesson 13

--- Gemfile
+++ Gemfile
@@ -69,6 +69,9 @@ source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
 end
 
+gem 'will_paginate', '>3.0'
+
+gem 'simple_form'
 
 
 
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,4 +2,13 @@ class PlacesController < ApplicationController
   def index
     @places = Place.all
   end
+
+  def index
+    @places = Place.order("name").page(params[:page]).per_page(5)
+  end
+
+  def new
+    @place = Place.new
+  end
+
 end
